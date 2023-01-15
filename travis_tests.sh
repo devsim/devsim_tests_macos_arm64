27c381ff37d4976c296b08a19de9368c1edd6ac7
Juan E. Sanchez <juan@tcad.com>
2023-01-15 19:17:56 +0000

    work on arm64 version

---
+++ travis_tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+BASEDIR="${PWD}"
+TAG=${1}
+TAGDIR=devsim_macos_${TAG}
+TAGTGZ=${TAGDIR}.tgz
+DEVSIM_LIB=${TAGDIR}/lib
+
+#curl -L -O https://github.com/devsim/devsim/releases/download/${TAG}/${TAGTGZ}
+#tar xzf ${TAGTGZ} 
+
+mkdir -p bin
+cat << EOF > bin/devsim_py38
+#!/bin/bash
+set -e
+progname="\$0"
+curdir=\`dirname "\${progname}"\`
+export PYTHONHASHSEED=0
+# sequential speeds up small examples
+export MKL_NUM_THREADS=1
+export PYTHONPATH="\${curdir}"/../${DEVSIM_LIB}
+python "\$@"
+EOF
+chmod +x bin/devsim_py38
+cp CMakeLists.txt ${TAGDIR}/
+rm -rf run && mkdir run
+(cd run && cmake -DDEVSIM_TEST_GOLDENDIR=${BASEDIR}/goldenresults -DDEVSIM_PY3_TEST_EXE=${BASEDIR}/bin/devsim_py38 ../${TAGDIR})
+(cd run && (ctest -j4 --no-compress-output -T Test || true))
+
